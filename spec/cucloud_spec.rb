7176e86cb28250ca01e2c4b7c6618c4edefd6257
Scott Ross <sross07@gmail.com>
2016-05-09 01:53:23 -0400

    setting up stub for aws sdk

--- spec/cucloud_spec.rb
+++ spec/cucloud_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Cucloud do
+  it "example of how to setup stubbing for S3 should pass" do
 
+    dummy_value = 'cu-awesome'
+
+    Aws.config[:s3] = {
+      stub_responses: {
+        list_buckets: { buckets:[{name:dummy_value}]}
+      }
+    }
+
+    expect(Aws::S3::Client.new.list_buckets.buckets.map(&:name)).to eq [dummy_value]
+  end
 end
