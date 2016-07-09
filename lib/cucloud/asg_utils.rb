1393cd831d70d81dddcec95a44fc0787e81a724c
Brett Haranin <bmh67@cornell.edu>
2016-07-09 12:59:01 -0400

    Added get_launch_config_by_name method + tests

--- lib/cucloud/asg_utils.rb
+++ lib/cucloud/asg_utils.rb
@@ -10,13 +10,22 @@ module Cucloud
 
     # Get an autoscale instance by group name
     # @param name [String] A single autoscale group name
-    # @return [Aws::AutoScaling::Types::AutoScalingGroup] AWS SDK autoscale group object matching provided group name
+    # @return [Aws::AutoScaling::Types::AutoScalingGroup] AWS SDK autoscale group struct
     def get_asg_by_name(name)
       # https://docs.aws.amazon.com/sdkforruby/api/Aws/AutoScaling/Client.html#describe_auto_scaling_groups-instance_method
       asg_desc = @asg.describe_auto_scaling_groups(auto_scaling_group_names: [name])
 
       # nil if not found -> we're accepting one name, so return first result
       asg_desc.auto_scaling_groups[0]
+    end
+
+    # get an instance of the launch configuration for a given autoscaling group
+    # @param launch_config_name [String] Name of launch configuration (from ASG)
+    # @return [Aws::AutoScaling::Types::LaunchConfiguration] AWS SDK Launch Configuration struct
+    def get_launch_config_by_name(launch_config_name)
+      # https://docs.aws.amazon.com/sdkforruby/api/Aws/AutoScaling/Client.html#describe_launch_configurations-instance_method
+      lc_desc = @asg.describe_launch_configurations(launch_configuration_names: [launch_config_name])
+      lc_desc.launch_configurations[0]
     end
   end
 end
--- spec/asg_utils_spec.rb
+++ spec/asg_utils_spec.rb
@@ -67,4 +67,79 @@ describe Cucloud::AsgUtils do
       expect(asg_util.get_asg_by_name('test-group').nil?).to eq true
     end
   end
+
+  context 'while launch configuration is stubbed out with matching search result' do
+    before do
+      asg_client.stub_responses(
+        :describe_launch_configurations,
+        next_token: nil,
+        launch_configurations: [{
+          launch_configuration_name: 'test-lc',
+          launch_configuration_arn: 'arn:test-lc',
+          image_id: 'i-testami',
+          key_name: 'test-key',
+          security_groups: ['test-sg-1', 'test-sg-2'],
+          classic_link_vpc_id: 'vpc-id',
+          classic_link_vpc_security_groups: ['classic-link-sg-1', 'classic-link-sg-2'],
+          user_data: 'blob of user data text',
+          instance_type: 't2.micro',
+          kernel_id: 'test-kernel-id',
+          ramdisk_id: 'test-ramdisk-id',
+          block_device_mappings: [
+            {
+              virtual_name: 'block-device-vn-1',
+              device_name: 'device-name-1',
+              ebs: {
+                snapshot_id: 'ebs-snapshot-1',
+                volume_size: 1000,
+                volume_type: 'ebs-volume-type',
+                delete_on_termination: true,
+                iops: 1000,
+                encrypted: true
+              },
+              no_device: false
+            }
+          ],
+          instance_monitoring: { enabled: true },
+          spot_price: 'spot price',
+          iam_instance_profile: 'iam profile',
+          created_time: Time.new(2016, 7, 9, 13, 30, 0),
+          ebs_optimized: true,
+          associate_public_ip_address: false,
+          placement_tenancy: 'placement tenancy'
+        }]
+      )
+    end
+
+    it "'get_launch_config_by_name' should return without an error" do
+      expect { asg_util.get_launch_config_by_name('test-lc') }.not_to raise_error
+    end
+
+    it "'get_launch_config_by_name' should return first result" do
+      expect(asg_util.get_launch_config_by_name('test-lc').launch_configuration_name.to_s).to eq 'test-lc'
+    end
+
+    it "'get_launch_config_by_name' should return type Aws::AutoScaling::Types::LaunchConfiguration" do
+      expect(asg_util.get_launch_config_by_name('test-lc').class.to_s)
+        .to eq 'Aws::AutoScaling::Types::LaunchConfiguration'
+    end
+  end
+
+  context 'while launch configuration is stubbed without matching result' do
+    before do
+      asg_client.stub_responses(
+        :describe_launch_configurations,
+        next_token: nil,
+        launch_configurations: []
+      )
+    end
+
+    it "'get_launch_config_by_name' should return without an error" do
+      expect { asg_util.get_launch_config_by_name('test-lc') }.not_to raise_error
+    end
+
+    it "'get_launch_config_by_name' should return nil" do
+      expect(asg_util.get_launch_config_by_name('test-lc').nil?).to eq true
+    end
+  end
 end
