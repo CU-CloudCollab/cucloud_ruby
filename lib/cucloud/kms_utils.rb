c3b4ca2ba9de504566c382a06180b3205914ffb0
Paul Allen <pea1@cornell.edu>
2017-02-20 10:16:51 -0500

    add yard documentation; fix tests

--- lib/cucloud/kms_utils.rb
+++ lib/cucloud/kms_utils.rb
@@ -1,16 +1,26 @@
 module Cucloud
-  # Utilities library for interacting with KMS
+  # Utilities library for interacting with KMS.
   class KmsUtils
+    class MissingKmsKey < StandardError
+    end
+
+    ENCRYPTED_SUFFIX = '_encrypted'.freeze
+    DECRYPTED_SUFFIX = '_decrypted'.freeze
+
     attr_accessor :kms_key_id
 
+    # Initialize the class optionally providing an existing Aws::KMS::Client
+    # @param kms_client [Aws::KMS::Client] optional
     def initialize(kms_client = Aws::KMS::Client.new)
       @kms = kms_client
     end
 
     # Decrypt the given Base64-strict-encoded ciphertext.
     # @param ciphertext [String]  encrypted and Base64 strict encoded string
-    # @return plaintext [String] descrypted string
+    # @return [String] decrypted string (i.e. plaintext)
     def decrypt(ciphertext)
+      return nil if ciphertext.nil?
+      return '' if ciphertext.empty?
       @kms.decrypt(ciphertext_blob: Base64.strict_decode64(ciphertext)).plaintext
     end
 
@@ -20,7 +30,181 @@ module Cucloud
     # @param key_id [String] KMS key id to use for encryption (optional)
     # @return [String] Encrypted and Base64 strict encoded ciphertext
     def encrypt(plaintext, key_id = @kms_key_id)
+      return nil if plaintext.nil?
+      return '' if plaintext.empty?
       Base64.strict_encode64(@kms.encrypt(key_id: key_id, plaintext: plaintext).ciphertext_blob)
+    end
+
+    # Process the given structure and decrypt the values of any
+    # attributes with names suffixed by "\_encrypted". For each such encrypted
+    # atttribute-value pair, adds a new attribute with suffix "_decrypted"
+    # and value consisting of the plaintext (i.e. decrypted value)
+    # of the encrypted value.
+    # @example
+    #   decrypt_struct({ x_encrypted: <encrypted_value> }) =>
+    #     { x_encrypted: <encrypted_value>, x_decrypted: <plaintext> }
+    #   decrypt_struct([{ x_encrypted: <encrypted_value> } ]) =>
+    #     [{ x_encrypted: <encrypted_value>, x_decrypted: <plaintext> }]
+    # @param main_node the structure (Hash, Array) to decrypt
+    # @return a copy of the structure with additional atttribute-value pairs for the decrypted values
+    def decrypt_struct(main_node)
+      return nil if main_node.nil?
+      return main_node if main_node.is_a?(String)
+      if main_node.is_a?(Hash)
+        new_hash = {}
+        main_node.each_pair do |key, value|
+          if key_to_decrypt?(key)
+            plaintext = decrypt(value)
+            new_hash[decrypted_key_label(key)] = plaintext
+            new_hash[key] = value
+          else
+            result = decrypt_struct(value)
+            new_hash[key] = result
+          end
+        end
+        return new_hash
+      elsif main_node.is_a?(Array)
+        new_array = []
+        main_node.each do |element|
+          result = decrypt_struct(element)
+          new_array << result
+        end
+        return new_array
+      else
+        return main_node
+      end
+    end
+
+    # Process the given structure and encrypt the values of any attributes
+    # with names suffixed by "_decrypted". For each such plaintext
+    # atttribute-value pair, adds a new attribute with suffix "\_encrypted"
+    # and value consisting of the encrypted value. The "_decrypted"
+    # atttribute-value pair is removed from the structure. Uses the
+    # provided the KMS key provided,
+    # or the KMS key configured at initialization if none is provided.
+    # @example
+    #   encrypt_struct({ x_decrypted: <plaintext> }) =>
+    #     { x_encrypted: <encrypted_value> }
+    #   encrypt_struct([{ x_decrypted: <plaintext> }]) =>
+    #     [{ x_encrypted: <encrypted_value> }]
+    # @param main_node the structure (Hash, Array) to encrypt_struct
+    # @param key_id [String] KMS key id to use for encryption (optional)
+    # @return a copy of the structure with decrypted atttribute-value pairs replaced by encrypted atttribute-value pairs
+    def encrypt_struct(main_node, key_id = @kms_key_id)
+      return nil if main_node.nil?
+      if main_node.is_a?(Hash)
+        new_hash = {}
+        remove_keys = []
+        main_node.each_pair do |key, value|
+          if key_to_encrypt?(key)
+            ciphertext = encrypt(value, key_id)
+            new_hash[encrypted_key_label(key)] = ciphertext
+            remove_keys << key
+          else
+            result = encrypt_struct(value, key_id)
+            new_hash[key] = result
+          end
+        end
+        main_node.merge!(new_hash)
+        main_node.delete_if do |key, _|
+          remove_keys.include?(key)
+        end
+        return main_node
+      elsif main_node.is_a?(Array)
+        main_node.map do |element|
+          encrypt_struct(element, key_id)
+        end
+      else
+        return main_node
+      end
+    end
+
+    # Read a JSON file and encrypt JSON attributes within it.
+    # JSON attributes having name suffixed by "_decrypted" will be encrypted and added to the
+    # JSON structure. The original "_decrypted" atttribute-value pairs are removed from the
+    # returned JSON and replaced with "\_encrypted" versions.
+    # @param filename the name of the source JSON file
+    # @param key_id [String] KMS key id to use for encryption (optional)
+    # @return JSON content of the file with encrypted atttribute-value pairs
+    def encrypt_json_file(filename, key_id = @kms_key_id)
+      require 'json'
+      encrypt_json(JSON.parse(File.read(filename)), key_id)
+    end
+
+    # Encrypt attribute values within the JSON structure.
+    # JSON attributes having name suffixed by "_decrypted" will be encrypted and added to the
+    # JSON structure. The original "_decrypted" atttribute-value pairs are removed from the
+    # returned JSON and replaced with "\_encrypted" versions.
+    #
+    # This function is particularly useful for encrypting custom JSON passed
+    # to OpsWorks stacks or recipes.
+    #
+    # If the JSON structure contains a top-level attribute named "kem_key_arn",
+    # that KMS key is used for encryption.
+    # @param json [JSON] JSON structure to be encrypted
+    # @param key_id [String] KMS key id to use for encryption (optional)
+    # @return JSON structure with decrypted atttribute-value pairs replaced by encrypted atttribute-value pairs
+    def encrypt_json(json, key_id = @kms_key_id)
+      # expect kms_key_arn at top level of file and
+      # use it if no key_id is passed in
+      if key_id.nil?
+        raise(
+          MissingKmsKey,
+          'No KMS key ID provided. Expecting \'kms_key_arn\' as top-level key in JSON'
+        ) unless json.key?('kms_key_arn')
+        key_id = json['kms_key_arn']
+      end
+      encrypt_struct(json, key_id)
+    end
+
+    # Read a JSON file and decrypt JSON attributes within it.
+    # JSON attributes having name suffixed by "\_encrypted" will be decrypted and added to the
+    # JSON structure. The original "\_encrypted" atttribute-value pairs are left intact in the
+    # returned JSON.
+    # @param filename the name of the source JSON file
+    # @return JSON content of the file with decrypted atttribute-value pairs added
+    def decrypt_json_file(filename)
+      require 'json'
+      decrypt_json(JSON.parse(File.read(filename)))
+    end
+
+    # Decrypt attributes within the provided JSON.
+    # JSON attributes having name suffixed by "\_encrypted" will be decrypted and added to the
+    # JSON structure. The original "\_encrypted" atttribute-value pairs are left intact in the
+    # returned JSON.
+    #
+    # This function is particularly useful for encrypting custom JSON passed
+    # to OpsWorks stacks or recipes.
+    # @param json [JSON] the JSON structure to decrypt
+    # @return JSON with decrypted atttribute-value pairs added
+    def decrypt_json(json)
+      decrypt_struct(json)
+    end
+
+    private
+
+    def key_to_decrypt?(key)
+      key.to_s.end_with?(ENCRYPTED_SUFFIX)
+    end
+
+    def key_to_encrypt?(key)
+      key.to_s.end_with?(DECRYPTED_SUFFIX)
+    end
+
+    def encrypted_key_label(key)
+      if key.is_a?(Symbol)
+        key.to_s.sub(DECRYPTED_SUFFIX, ENCRYPTED_SUFFIX).to_sym
+      else
+        key.sub(DECRYPTED_SUFFIX, ENCRYPTED_SUFFIX)
+      end
+    end
+
+    def decrypted_key_label(key)
+      if key.is_a?(Symbol)
+        key.to_s.sub(ENCRYPTED_SUFFIX, DECRYPTED_SUFFIX).to_sym
+      else
+        key.sub(ENCRYPTED_SUFFIX, DECRYPTED_SUFFIX)
+      end
     end
   end
 end
--- spec/kms_utils_spec.rb
+++ spec/kms_utils_spec.rb
@@ -21,6 +21,39 @@ describe Cucloud::KmsUtils do
     Base64.strict_encode64(plaintext)
   end
 
+  let(:testcases1) do
+    [
+      nil, '', 1, 'string', {}, []
+    ]
+  end
+
+  let(:testcases2) do
+    [
+      { 'key1' => 'value1' },
+      { key1: 'value1' },
+      { 'key1' => 'value1' },
+      { 'key1_decrypted' => 'value1' },
+      { 'key1' => %w('value1 value2') },
+      { 'level1' => { 'key1' => 'value1' } },
+      { 'level1' => { 'level2' => { 'key1' => 'value1' } } },
+      { 'level1' => { 'level2' => { 'level3' => { 'key1' => 'value1' } } } },
+      { 'key9' => 'value9',
+        'level1' => { 'key9' => 'value9', 'key1' => 'value1' } },
+      { 'key9' => 'value9',
+        'level1' => { 'key9' => 'value9',
+                      'level2' => { 'key9' => 'value9', 'key1' => 'value1' } } },
+      { 'key9' => 'value9',
+        'level1' => { 'key9' => 'value9',
+                      'level2' => { 'key9' => 'value9',
+                                    'level3' => { 'key1' => 'value1' } } } },
+      ['value1'],
+      %w('value1 value2'),
+      ['value1', %w('value2 value3')],
+      [{ 'key1' => 'value1' }],
+      [{ 'key1' => 'value1' }, { 'key2' => 'value2' }]
+    ]
+  end
+
   it '.new default optional should be successful' do
     expect(Cucloud::KmsUtils.new).to be_a_kind_of(Cucloud::KmsUtils)
   end
@@ -30,8 +63,12 @@ describe Cucloud::KmsUtils do
   end
 
   describe '#decrypt' do
-    it 'should raise an error when ciphertext is nil' do
-      expect { kms_util.decrypt(nil) }.to raise_error(NoMethodError)
+    it 'should return nil when ciphertext is nil' do
+      expect(kms_util.decrypt(nil)).to be_nil
+    end
+
+    it 'should return emptry string when ciphertext is emptry string' do
+      expect(kms_util.decrypt('')).to eq('')
     end
 
     it 'should raise an error when ciphertext is not valid Base64 string encoded' do
@@ -42,7 +79,8 @@ describe Cucloud::KmsUtils do
       kms_client.stub_responses(
         :decrypt,
         plaintext: plaintext,
-        key_id: key_id)
+        key_id: key_id
+      )
     end
 
     it 'should return plaintext when an encrypted value is passed' do
@@ -51,11 +89,13 @@ describe Cucloud::KmsUtils do
   end
 
   describe '#encrypt' do
-
-    it 'should raise an error when plaintext is nil' do
-      expect { kms_util.encrypt(nil) }.to raise_error(ArgumentError)
+    it 'should return nil when plaintext is nil' do
+      expect(kms_util.encrypt(nil)).to be_nil
     end
 
+    it 'should return empty string when plaintext is empty string' do
+      expect(kms_util.encrypt('')).to eq('')
+    end
   end
 
   context 'while KmsUtils has no kms_key_id set' do
@@ -76,7 +116,8 @@ describe Cucloud::KmsUtils do
         kms_client.stub_responses(
           :encrypt,
           ciphertext_blob: plaintext,
-          key_id: key_id)
+          key_id: key_id
+        )
       end
 
       it 'should return ciphertext when a kms_key_id is passed' do
@@ -107,7 +148,8 @@ describe Cucloud::KmsUtils do
         kms_client.stub_responses(
           :encrypt,
           ciphertext_blob: plaintext,
-          key_id: key_id)
+          key_id: key_id
+        )
       end
 
       it 'should return ciphertext when no kms_key_id is passed' do
@@ -118,7 +160,123 @@ describe Cucloud::KmsUtils do
         expect(kms_util.encrypt(plaintext, key_id)).to eq(ciphertext)
       end
     end
+
+    describe '#encrypt_struct' do
+      it 'should return degenerate objects without change' do
+        testcases1.each do |item|
+          expect(kms_util.encrypt_struct(item)).to eq(item)
+        end
+      end
+
+      it 'should return objects not containing a hash key with suffix _decrypted without change' do
+        testcases2.each do |item|
+          expect(kms_util.encrypt_struct(item)).to eq(item)
+        end
+      end
+
+      before do
+        kms_client.stub_responses(
+          :encrypt,
+          ciphertext_blob: plaintext,
+          key_id: key_id
+        )
+      end
+
+      let(:testcases3) do
+        [
+          { input: { 'key1_decrypted' => plaintext }, output: { 'key1_encrypted' => ciphertext } },
+          { input: { key1_decrypted: plaintext }, output: { key1_encrypted: ciphertext } },
+          { input: { key1_encrypted: ciphertext }, output: { key1_encrypted: ciphertext } }
+        ]
+      end
+
+      it 'should encrypt values for keys with suffix _decrypted' do
+        testcases3.each do |testcase|
+          expect(kms_util.encrypt_struct(testcase[:input])).to include(testcase[:output])
+        end
+      end
+
+      let(:testcases4) do
+        [
+          { input: { level1: { key1_decrypted: plaintext } }, output: { level1: { key1_encrypted: ciphertext } } },
+          { input: { level1: { level2: { key1_decrypted: plaintext } } },
+            output: { level1: { level2: { key1_encrypted: ciphertext } } } },
+          { input: { level1: 'nothing', key1_decrypted: plaintext },
+            output: { level1: 'nothing', key1_encrypted: ciphertext } },
+          { input: ['nothing', { level1: { key1_decrypted: plaintext } }],
+            output: { level1: { key1_encrypted: ciphertext } } }
+        ]
+      end
+      it 'should encrypt values for keys with suffix _decrypted and maintain original object structure' do
+        testcases4.each do |testcase|
+          expect(kms_util.encrypt_struct(testcase[:input])).to include(testcase[:output])
+        end
+      end
+
+      let(:testcases5) do
+        [
+          { input: { key1_decrypted: plaintext }, output: :key_decrypted },
+          { input: { key1_encrypted: ciphertext }, output: :key_decrypted }
+        ]
+      end
+      it 'should encrypt values for keys with suffix _decrypted and remove original key' do
+        testcases5.each do |testcase|
+          expect(kms_util.encrypt_struct(testcase[:input])).not_to include(testcase[:output])
+        end
+      end
+    end
+
+    describe '#decrypt_struct' do
+      it 'should return degenerate objects without change' do
+        testcases1.each do |item|
+          expect(kms_util.decrypt_struct(item)).to eq(item)
+        end
+      end
+
+      it 'should return objects not containing a hash key with suffix _encrypted without change' do
+        testcases2.each do |item|
+          expect(kms_util.decrypt_struct(item)).to eq(item)
+        end
+      end
+
+      before do
+        kms_client.stub_responses(
+          :decrypt,
+          plaintext: plaintext,
+          key_id: key_id
+        )
+      end
+
+      let(:testcases3) do
+        [
+          { input: { 'key1_encrypted' => ciphertext }, output: { 'key1_decrypted' => plaintext } },
+          { input: { key1_encrypted: ciphertext }, output: { key1_decrypted: plaintext } },
+          { input: { key1_decrypted: plaintext }, output: { key1_decrypted: plaintext } }
+        ]
+      end
+      it 'should decrypt values for keys with suffix _encrypted' do
+        testcases3.each do |testcase|
+          expect(kms_util.decrypt_struct(testcase[:input])).to include(testcase[:output])
+        end
+      end
+
+      let(:testcases4) do
+        [
+          { input: { level1: { key1_encrypted: ciphertext } },
+            output: { level1: { key1_decrypted: plaintext, key1_encrypted: ciphertext } } },
+          { input: { level1: { level2: { key1_encrypted: ciphertext } } },
+            output: { level1: { level2: { key1_decrypted: plaintext, key1_encrypted: ciphertext } } } },
+          { input: { level1: 'nothing', key1_encrypted: ciphertext },
+            output: { level1: 'nothing', key1_decrypted: plaintext, key1_encrypted: ciphertext } },
+          { input: ['nothing', { level1: { key1_encrypted: ciphertext } }],
+            output: { level1: { key1_decrypted: plaintext, key1_encrypted: ciphertext } } }
+        ]
+      end
+      it 'should decrypt values for keys with suffix _encrypted and add to original object structure' do
+        testcases4.each do |testcase|
+          expect(kms_util.decrypt_struct(testcase[:input])).to include(testcase[:output])
+        end
+      end
+    end
   end
-
-
 end
